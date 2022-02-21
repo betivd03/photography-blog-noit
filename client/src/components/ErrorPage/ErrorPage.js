ca91d56f38a1ded201eaeed51262b9e30dfd1ca2
Beatris Borislavova <betivd03@abv.bg>
2022-02-21 20:49:28 +0200

    Add Error Page component

---
+++ client/src/components/ErrorPage/ErrorPage.js
@@ -0,0 +1,12 @@
+const ErrorPage = () => {
+    return (
+        <section id="error-page">
+            <header>
+                <h1 id="error-page-header">404 Not Found!</h1>
+                <p>Something went wrong!</p>
+            </header>
+        </section>
+    );
+};
+
+export default ErrorPage;
