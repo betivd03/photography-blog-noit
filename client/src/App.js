9e2fea105c2d92bfed1f185984ef3bb3f7eaa22c
Beatris Borislavova <betivd03@abv.bg>
2022-02-21 20:50:25 +0200

    Update App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,10 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Login from './components/Login';
 import Register from "./components/Register";
+import AddPhoto from "./components/AddPhoto";
+import EditPhoto from "./components/EditPhoto";
+import PhotoDetails from "./components/PhotoDetails";
+import ErrorPage from "./components/ErrorPage";
 
 function App() {
     return (
@@ -16,6 +20,10 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/add" element={<AddPhoto />} />
+                <Route path="/edit" element={<EditPhoto />} />
+                <Route path="/details" element={<PhotoDetails />} />
+                <Route path="/error" element={<ErrorPage />} />
             </Routes>
         </div>
     );
