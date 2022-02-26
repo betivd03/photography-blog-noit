8a91b3d212d5191d84d470748f18f3af3a0d6eeb
Beatris Borislavova <betivd03@abv.bg>
2022-02-26 19:51:45 +0200

    Update Home component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -6,23 +6,29 @@ const Home = () => {
     return (
         <Carousel fade className="carousel h-100">
             <Carousel.Item className="carousel-item">
-                <img
-                    className="d-block carousel-img h-100"
-                    src="/images/photography-blog-logo.jpg"
-                    alt="First slide"
-                />
+                <div className="img-container">
+                    <img
+                        className="d-block carousel-img"
+                        src="/images/photography-blog-logo.jpg"
+                        alt="First slide"
+                    />
+                    <div className="overlay"></div>
+                </div>
                 <Carousel.Caption>
                     <h3>First slide label</h3>
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                 </Carousel.Caption>
             </Carousel.Item>
-            
+
             <Carousel.Item className="w-100 carousel-item">
-                <img
-                    className="d-block carousel-img h-100"
-                    src="/images/ocean.jpg"
-                    alt="Second slide"
-                />
+                <div className="img-container">
+                    <img
+                        className="d-block carousel-img"
+                        src="/images/ocean.jpg"
+                        alt="Second slide"
+                    />
+                    <div className="overlay"></div>
+                </div>
                 <Carousel.Caption>
                     <h3>Second slide label</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -30,11 +36,14 @@ const Home = () => {
             </Carousel.Item>
 
             <Carousel.Item className="w-100 carousel-item">
-                <img
-                    className="d-block carousel-img h-100"
-                    src="/images/ocean-photography.jpg"
-                    alt="Third slide"
-                />
+                <div className="img-container">
+                    <img
+                        className="d-block carousel-img"
+                        src="/images/ocean-photography.jpg"
+                        alt="Third slide"
+                    />
+                    <div className="overlay"></div>
+                </div>
                 <Carousel.Caption>
                     <h3>Third slide label</h3>
                     <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
