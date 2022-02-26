50360217317dc2338e4533463351b12df3918de2
Beatris Borislavova <betivd03@abv.bg>
2022-02-26 20:43:04 +0200

    Add Edit Photo functionality

--- client/src/components/EditPhoto/EditPhoto.js
+++ client/src/components/EditPhoto/EditPhoto.js
@@ -1,29 +1,85 @@
 import { Form, Button } from 'react-bootstrap';
 import "./EditPhoto.css";
 
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext.js";
+
+import * as photosService from "../../services/photosService.js";
+
 const AddPhoto = () => {
+
+    const navigate = useNavigate();
+
+    const { user } = useContext(AuthContext); 
+
+    const { photoId } = useParams();
+
+    const [photo, setPhoto] = useState([]);
+
+    const [error, setError] = useState([]);
+
+    useEffect(() => {
+        photosService.getOne(photoId)
+            .then(res => {
+                setPhoto(res);
+            })
+    }, []);
+
+    const onEditSubmitHandler = (e) => {
+        e.preventDefault();
+
+        let formData = new FormData(e.currentTarget);
+        let { title, category, description, location } = Object.fromEntries(formData);
+
+        if (title === '' || category === '' || description === '' || location === '') {
+            return setError('All fields are required!');
+        } else if (title.length < 3) {
+            return setError('Title must be at least 3 characters long!');
+        } else if (category.length < 3) {
+            return setError('Category must be at least 3 characters long!');
+        } else if (description.length < 3) {
+            return setError('Description must be at least 3 characters long!');
+        } else if (location.length < 3) {
+            return setError('Location must be at least 3 characters long!');
+        }
+
+        photosService.edit({
+            title,
+            category,
+            description,
+            location
+        }, photoId, user.accessToken)
+            .then(() => {
+                navigate(`/details/${photoId}`);
+            });
+    };
 
     return (
         <div className="edit-photo-div">
-            <Form className="w-50 edit-photo-form">
+            {error.length === 0 
+                ? <div className="hidden"></div>
+                : <div className="error-div"><p>{error}</p></div>}
+
+            <Form className="w-50 edit-photo-form" method="POST" onSubmit={onEditSubmitHandler}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label className="label-style">Title</Form.Label>
-                    <Form.Control type="text" placeholder="Enter title" />
+                    <Form.Control type="text" id="title" name="title" placeholder="Enter title" defaultValue={photo.title} />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label className="label-style">Category</Form.Label>
-                    <Form.Control type="text" placeholder="Enter category" />
+                    <Form.Control type="text" id="category" name="category" placeholder="Enter category" defaultValue={photo.category} />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label className="label-style">Description</Form.Label>
-                    <Form.Control type="text" placeholder="Enter description" />
+                    <Form.Control type="text" id="description" name="description" placeholder="Enter description" defaultValue={photo.description} />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label className="label-style">Location</Form.Label>
-                    <Form.Control type="text" placeholder="Enter location" />
+                    <Form.Control type="text" id="location" name="location" placeholder="Enter location" defaultValue={photo.location} />
                 </Form.Group>
 
                 <Form.Group className="position-relative mb-3">
