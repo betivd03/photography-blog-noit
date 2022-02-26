d2b1d849452f334c2d2e68ce0e94f104419bd965
Beatris Borislavova <betivd03@abv.bg>
2022-02-26 19:51:35 +0200

    Update Header component

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,25 +5,27 @@ import "./Header.css";
 const Header = () => {
 
     return (
-        <Navbar className="navbar" bg="light" expand="lg">
+        <Navbar className="navbar" expand="lg">
             <Container fluid className="navbar-container">
                 <Navbar.Brand href="/">
                     <img
-                        src="/images/blog-logo.png"
-                        width="300"
-                        height="70"
-                        className="d-inline-block align-top"
+                        src="/images/photography_blog_logo.png"
+                        width="200"
+                        height="90"
+                        className="d-inline-block align-top blog-logo"
                         alt="Photography Blog logo"
                     />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
-                        className="me-auto my-2 my-lg-0"
+                        className="me-auto my-2 my-lg-0 navigation"
                         style={{ maxHeight: '100px' }}
                         navbarScroll
                     >
                         <Nav.Link className="nav-link" to="/" as={Link}>Home</Nav.Link>
+                        <Nav.Link className="nav-link" to="/gallery" as={Link}>Gallery</Nav.Link>
+                        <Nav.Link className="nav-link" to="/about" as={Link}>About</Nav.Link>
                         <Nav.Link className="nav-link" to="/login" as={Link}>Login</Nav.Link>
                         <Nav.Link className="nav-link" to="/register" as={Link}>Register</Nav.Link>
                         <Nav.Link className="nav-link" to="/add" as={Link}>Add Photo</Nav.Link>
