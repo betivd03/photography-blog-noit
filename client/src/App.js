77445b8ac68f99393389c435b1ff02527627327c
Beatris Borislavova <betivd03@abv.bg>
2022-02-26 19:48:37 +0200

    Add main and footer

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from './components/Header';
 import Home from './components/Home';
+import Gallery from "./components/Gallery";
+import About from "./components/About";
 import Login from './components/Login';
 import Register from "./components/Register";
 import AddPhoto from "./components/AddPhoto";
@@ -13,18 +15,22 @@ import ErrorPage from "./components/ErrorPage";
 
 function App() {
     return (
-        <div>
+        <div className="layout">
             <Header />
-
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/add" element={<AddPhoto />} />
-                <Route path="/edit" element={<EditPhoto />} />
-                <Route path="/details" element={<PhotoDetails />} />
-                <Route path="/error" element={<ErrorPage />} />
-            </Routes>
+            <main className="app-main">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/gallery" element={<Gallery />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/add" element={<AddPhoto />} />
+                    <Route path="/edit" element={<EditPhoto />} />
+                    <Route path="/details" element={<PhotoDetails />} />
+                    <Route path="/error" element={<ErrorPage />} />
+                </Routes>
+            </main>
+            <footer>All rights reserved &copy;</footer>
         </div>
     );
 }
