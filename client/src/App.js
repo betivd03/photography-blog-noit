fe69d64874da2bffc0cd0e99ee3b7ce9d94a960a
Beatris Borislavova <betivd03@abv.bg>
2022-02-26 20:09:47 +0200

    Add AuthContext, useLocalStorage and initialAuth in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import { Routes, Route } from "react-router-dom";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { AuthContext } from "./contexts/AuthContext.js";
+import useLocalStorage from "./hooks/useLocalStorage.js";
+
 import Header from './components/Header';
 import Home from './components/Home';
 import Gallery from "./components/Gallery";
@@ -13,25 +16,49 @@ import EditPhoto from "./components/EditPhoto";
 import PhotoDetails from "./components/PhotoDetails";
 import ErrorPage from "./components/ErrorPage";
 
+const initialAuth = {
+    email: "",
+    username: "",
+    token: "",
+    _id: ""
+};
+
 function App() {
+
+    const [user, setUser] = useLocalStorage('user', initialAuth);
+
+    const login = (authData) => {
+        setUser(authData);
+    };
+
+    const register = (authData) => {
+        setUser(authData);
+    };
+
+    const logout = () => {
+        setUser(initialAuth);
+    };
+
     return (
-        <div className="layout">
-            <Header />
-            <main className="app-main">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/gallery" element={<Gallery />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/add" element={<AddPhoto />} />
-                    <Route path="/edit" element={<EditPhoto />} />
-                    <Route path="/details" element={<PhotoDetails />} />
-                    <Route path="/error" element={<ErrorPage />} />
-                </Routes>
-            </main>
-            <footer>All rights reserved &copy;</footer>
-        </div>
+        <AuthContext.Provider value={{user, login, register, logout}} >
+            <div className="layout">
+                <Header />
+                <main className="app-main">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/gallery" element={<Gallery />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/add" element={<AddPhoto />} />
+                        <Route path="/edit" element={<EditPhoto />} />
+                        <Route path="/details" element={<PhotoDetails />} />
+                        <Route path="/error" element={<ErrorPage />} />
+                    </Routes>
+                </main>
+                <footer>All rights reserved &copy;</footer>
+            </div>
+        </AuthContext.Provider>
     );
 }
 
