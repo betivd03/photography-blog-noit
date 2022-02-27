57072c34ecae66af7e9c34652c98304c6b10b1f7
Beatris Borislavova <betivd03@abv.bg>
2022-02-27 22:20:40 +0200

    Add My Profile component

---
+++ client/src/components/MyProfile/MyPhotoCard/MyPhotoCard.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+import "./MyPhotoCard.css";
+
+const MyPhotoCard = ({
+    photo
+}) => {
+    return (
+        <div className="my-photo-card">
+            <h3>{photo.title}</h3>
+            <div className="image">
+                <img src={photo.imageUrl} alt={photo.title} />
+            </div>
+            <Link to={`/details/${photo._id}`}>Details</Link>
+        </div>
+    );
+};
+
+export default MyPhotoCard;
---
+++ client/src/components/MyProfile/MyProfile.js
@@ -0,0 +1,50 @@
+import { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext.js";
+
+import "./MyProfile.css";
+
+import MyPhotoCard from "./MyPhotoCard";
+
+import * as photosService from "../../services/photosService.js";
+
+const MyProfile = () => {
+    const [myPhotos, setMyPhotos] = useState([]);
+
+    const { user } = useContext(AuthContext);
+
+    useEffect(() => {
+        photosService.getMy(user._id)
+            .then(result => {
+                console.log(user._id);
+                setMyPhotos(result);
+            })
+    }, []);
+
+    return (
+        <section id="my-profile-page">
+            <div className="profile-info">
+                <div className="profile-photo">
+                    <img src="/images/profile-photo.jpg" alt="Profile" />
+                </div>
+                <div className="user-info">
+                    <p>{user.username}</p>
+                </div>
+            </div>
+            <hr />
+
+            {myPhotos.length > 0
+                ? (
+                    <div className="my-photos">
+                        {myPhotos.map(x => <MyPhotoCard key={x._id} photo={x} />)}
+                    </div>
+                )
+                : (
+                    <p>No Photos Yet!</p>
+                )
+            }
+            
+        </section>
+    );
+};
+
+export default MyProfile;
