8ef0315e9ca1efd2f14c91ec21e33ccd0cccefbf
Beatris Borislavova <betivd03@abv.bg>
2022-03-01 00:16:46 +0200

    Connect with MongoDB Atlas

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose');
 const { DB_URI, ATLAS_URI } = require('./constants.js');
 
 exports.initDatabase = function() {
-    return mongoose.connect(DB_URI);
+    return mongoose.connect(ATLAS_URI);
 };
