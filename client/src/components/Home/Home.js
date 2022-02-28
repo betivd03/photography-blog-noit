e088f36e70562596e160e67f795cab2ec74c5dc6
Beatris Borislavova <betivd03@abv.bg>
2022-03-01 00:09:59 +0200

    Update Home component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,34 +1,54 @@
 import { Carousel } from 'react-bootstrap';
 import "./Home.css";
 
-import { useState, useEffect } from "react";
-
-import CarouselItem from "./CarouselItem.js";
-
-import * as photosService from "../../services/photosService.js";
-
 const Home = () => {
-
-    const [photos, setPhotos] = useState([]);
-
-    useEffect(() => {
-        photosService.getLatest()
-            .then(result => setPhotos(result));
-    }, []);
-
     return (
         <Carousel fade className="carousel h-100">
             
-        {photos.length > 0
-            ? (
-                <>
-                    {photos.map(x => <CarouselItem key={x._id} photo={x} />)}
-                </>
-            )
-            : (
-                <p>No Photos Yet!</p>
-            )
-        }
+            <Carousel.Item className="carousel-item">
+                <div className="img-container">
+                    <img
+                        className="d-block carousel-img"
+                        src="/images/ocean-photography.jpg"
+                        alt="Ocean Photography"
+                    />
+                    <div className="overlay"></div>
+                </div>
+                <Carousel.Caption>
+                    <h3>Welcome to Photography Blog!</h3>
+                    <p>The best blog for sharing professional photos!</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item className="carousel-item">
+                <div className="img-container">
+                    <img
+                        className="d-block carousel-img"
+                        src="/images/wildlife-photography.jpg"
+                        alt="Wildlife Photography"
+                    />
+                    <div className="overlay"></div>
+                </div>
+                <Carousel.Caption>
+                    <h3>Get inspired by our photos!</h3>
+                    <p>In our gallery you will find photos from different categories!</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item className="carousel-item">
+                <div className="img-container">
+                    <img
+                        className="d-block carousel-img"
+                        src="/images/city-photography.jpg"
+                        alt="City Photography"
+                    />
+                    <div className="overlay"></div>
+                </div>
+                <Carousel.Caption>
+                    <h3>Join our blog!</h3>
+                    <p>Register and post your own professional photos!</p>
+                </Carousel.Caption>
+            </Carousel.Item>
             
         </Carousel>
     );
