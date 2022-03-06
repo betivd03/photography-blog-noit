1d55834dca095c29ca9a3c7a04a575857b9fb40a
Beatris Borislavova <betivd03@abv.bg>
2022-03-06 18:47:54 +0200

    Update constants.js

--- server/constants.js
+++ server/constants.js
@@ -1,6 +1,6 @@
 exports.PORT = 5000;
 exports.DB_URI = 'mongodb://localhost:27017/test';
-exports.ATLAS_URI = 'mongodb+srv://beti:85PGrAQctw0teyQo@test.tj6lw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 exports.SALT_ROUNDS = 10;
 exports.JWT_SECRET = 'CD3C62A73447EB4BEFDD99F459573';
 exports.AUTH_COOKIE_NAME = 'token';
