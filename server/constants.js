1dce4b4ff5dcd9de71cc7f63fd4a6f68aced89be
Beatris Borislavova <betivd03@abv.bg>
2022-03-06 22:47:22 +0200

    Restore constants.js

---
+++ server/constants.js
@@ -0,0 +1,7 @@
+exports.PORT = process.env.PORT || 5000;
+exports.DB_URI = 'mongodb://localhost:27017/test';
+// exports.ATLAS_URI = 'mongodb+srv://beti:85PGrAQctw0teyQo@test.tj6lw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+exports.ATLAS_URI = 'mongodb+srv://BettyB11:OHYTeljxFRgnJGID@photography-blog-noit.i2bsv.mongodb.net/photography-blog-noit?retryWrites=true&w=majority';
+exports.SALT_ROUNDS = 10;
+exports.JWT_SECRET = 'CD3C62A73447EB4BEFDD99F459573';
+exports.AUTH_COOKIE_NAME = 'token';
