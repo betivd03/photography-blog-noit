6ba5e4f789ca06d7b7f4e1170f6bc0a1b83389d2
Beatris Borislavova <betivd03@abv.bg>
2022-03-06 22:40:22 +0200

    Initial commit

--- server/constants.js
+++
@@ -1,6 +0,0 @@
-exports.PORT = 5000;
-exports.DB_URI = 'mongodb://localhost:27017/test';
-
-exports.SALT_ROUNDS = 10;
-exports.JWT_SECRET = 'CD3C62A73447EB4BEFDD99F459573';
-exports.AUTH_COOKIE_NAME = 'token';
